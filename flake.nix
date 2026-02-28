44afe34762370470e08e9ad3133e401e7b11fa29
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-28 15:31:42 +0200

    chore: bump version in flake

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
         packages.spyglass = pkgs.buildGoModule {
           pname = "spyglass";
-          version = "2026.02.28-a";
+          version = "2026.02.28-c";
 
           src = self;
 
